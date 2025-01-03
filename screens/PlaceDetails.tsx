8b70ccc908ea2da53ce544fcdf5d472e1e3e3e19
Badi1298 <serbandavid83@yahoo.com>
2025-01-03 13:11:39 +0200

    Add initial screen components: AddPlace, Map, and PlaceDetails

---
+++ screens/PlaceDetails.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function PlaceDetails() {
+    return (
+        <View>
+            <Text>PlaceDetails</Text>
+        </View>
+    );
+}
