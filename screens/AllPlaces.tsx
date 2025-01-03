b711592a6a0c22cc2b832cd4daec3dd4b165a1dd
Badi1298 <serbandavid83@yahoo.com>
2025-01-03 13:09:57 +0200

    first commit

---
+++ screens/AllPlaces.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function AllPlaces() {
+    return (
+        <View>
+            <Text>AllPlaces</Text>
+        </View>
+    );
+}
